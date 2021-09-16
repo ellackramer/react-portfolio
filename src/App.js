7126efdfd709a3af7b11fd9c5322195552f74380
ellackramer <ellackramer@gmail.com>
2021-09-16 12:37:53 -0400

    building out react skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return "App"; (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  )
+}
